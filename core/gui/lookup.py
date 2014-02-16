7108725946d535856465ecf008ee3e8efd6fa801
Virgil Dupras <hsoft@hardcoded.net>
2014-02-16 11:08:20 -0500

    Documented core.gui.lookup

--- core/gui/lookup.py
+++ core/gui/lookup.py
@@ -35,12 +35,36 @@ def letters_distance(s, query):
     return sum(letter_distance(s, l1, l2) for l1, l2 in combinations(query, 2))
 
 class Lookup:
+    """Fuzzily-filtered list of items that match the few letters that the user typed.
+
+    A Lookup allows the user to quickly find a object he looks for by name by typing only a few
+    letters from that name. When first invoked, we start with a list of all possible items, and as
+    letters are typed, those items and filtered *and* ordered in a way that fuzzily matches what
+    our user typed.
+
+    The list's filter is very inclusive and will include object that very remotely can match user's
+    input, but the order is based on what matches the most.
+
+    For example, if the user types "ab", the lookup list would contain both "barber", "absolute", 
+    and "albinos" (all those names contain both "a" and "b"), but would be ordered so that
+    "absolute" comes first because it's the closest match.
+
+    Pressing Return will "activate" (see :meth:`go`) the first item in the list, but the user can
+    also navigate the list with the arrow keys (or click on the wanted item), which is why we keep
+    track of :attr:`selected_index`.
+
+    This class is designed to be subclassed with concrete named objects to lookup in. It does
+    nothing by itself.
+    """
     def __init__(self, mainwindow):
+        #: :class:`.MainWindow`
         self.mainwindow = mainwindow
+        #: :class:`.Document`
         self.document = mainwindow.document
         self._original_names = []
         self._filtered_names = []
         self._search_query = ''
+        #: *int*. Currently selected index in the filtered/ordered list.
         self.selected_index = 0
         self.view = NoopGUI()
     
@@ -57,11 +81,16 @@ class Lookup:
         self.selected_index = max(self.selected_index, 0)
         self.selected_index = min(self.selected_index, len(self._filtered_names)-1)
     
-    def _generate_lookup_names(self): # Virtual
+    def _generate_lookup_names(self):
+        """*Virtual*. Return a list of names in which we'll search."""
         return []
     
-    def _go(self, name): #Virtual
-        pass
+    def _go(self, name):
+        """*Virtual*. Select an item named ``name``.
+
+        The nature of this action depends on what kind of object we perform the lookup on. Were
+        those accounts? Then open up the account named ``name``.
+        """
     
     def _refresh(self):
         self._search_query = ''
@@ -75,6 +104,10 @@ class Lookup:
         self._filtered_names = normalized_names
     
     def go(self):
+        """Activate the :attr:`selected item <selected_index>` and close the lookup.
+
+        To "activate", we call :meth:`_go` with the name of our selected item.
+        """
         try:
             name = self.names[self.selected_index]
             self._go(name)
@@ -83,6 +116,7 @@ class Lookup:
         self.view.hide()
     
     def show(self):
+        """Refreshes the name list and show the lookup."""
         self._refresh()
         self.view.refresh()
         self.view.show()
@@ -90,10 +124,12 @@ class Lookup:
     #--- Properties
     @property
     def names(self):
+        """List of filtered/ordered names based on :attr:`search_query`."""
         return [self._normalized2original[n] for n in self._filtered_names]
     
     @property
     def search_query(self):
+        """*get/set*. *str*. Search query typed by the user."""
         return self._search_query
     
     @search_query.setter
---
+++ help/en/developer/core/gui/lookup.rst
@@ -0,0 +1,6 @@
+core.gui.lookup
+===============
+
+.. automodule:: core.gui.lookup
+    :members:
+    :private-members:
