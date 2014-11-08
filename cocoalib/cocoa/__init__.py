983766c24a63ed7eecd12ad17ad38f29d3668ff8
Virgil Dupras <hsoft@hardcoded.net>
2014-11-08 14:07:20 -0500

    Updated cocoalib

--- cocoalib/cocoa/__init__.py
+++ cocoalib/cocoa/__init__.py
@@ -2,17 +2,15 @@
 # Created On: 2007-10-06
 # Copyright 2014 Hardcoded Software (http://www.hardcoded.net)
 
-# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.hardcoded.net/licenses/bsd_license
 
 import logging
 import time
 import traceback
-import subprocess
 import sys
 
-from hscommon.error_report import send_error_report
 from .CocoaProxy import CocoaProxy
 
 proxy = CocoaProxy()
@@ -28,7 +26,7 @@ def autoreleasepool(func):
 
 def as_fetch(as_list, as_type, step_size=1000):
     """When fetching items from a very big list through applescript, the connection with the app
-    will timeout. This function is to circumvent that. 'as_type' is the type of the items in the 
+    will timeout. This function is to circumvent that. 'as_type' is the type of the items in the
     list (found in appscript.k). If we don't pass it to the 'each' arg of 'count()', it doesn't work.
     applescript is rather stupid..."""
     result = []
@@ -68,7 +66,7 @@ def extract_tb_noline(tb):
 
 def safe_format_exception(type, value, tb):
     """Format exception from type, value and tb and fallback if there's a problem.
-    
+
     In some cases in threaded exceptions under Cocoa, I get tracebacks targeting pyc files instead
     of py files, which results in traceback.format_exception() trying to print lines from pyc files
     and then crashing when trying to interpret that binary data as utf-8. We want a fallback in
@@ -82,31 +80,31 @@ def safe_format_exception(type, value, tb):
         result.extend(traceback.format_exception_only(type, value))
         return result
 
-def report_crash(type, value, tb):
-    app_identifier = proxy.bundleIdentifier()
-    app_version = proxy.appVersion()
-    osx_version = proxy.osxVersion()
-    s = "Application Identifier: {}\n".format(app_identifier)
-    s += "Application Version: {}\n".format(app_version)
-    s += "Mac OS X Version: {}\n\n".format(osx_version)
-    s += ''.join(safe_format_exception(type, value, tb))
-    if app_identifier:
-        s += '\nRelevant Console logs:\n\n'
-        p = subprocess.Popen(['grep', app_identifier, '/var/log/system.log'], stdout=subprocess.PIPE)
-        try:
-            s += str(p.communicate()[0], encoding='utf-8')
-        except IndexError:
-            # This can happen if something went wrong with the grep (permission errors?)
-            pass
-    if proxy.reportCrash_(s):
-        send_error_report(s)
+def install_exception_hook(github_url):
+    def report_crash(type, value, tb):
+        app_identifier = proxy.bundleIdentifier()
+        app_version = proxy.appVersion()
+        osx_version = proxy.osxVersion()
+        s = "Application Identifier: {}\n".format(app_identifier)
+        s += "Application Version: {}\n".format(app_version)
+        s += "Mac OS X Version: {}\n\n".format(osx_version)
+        s += ''.join(safe_format_exception(type, value, tb))
+        if LOG_BUFFER:
+            s += '\nRelevant Console logs:\n\n'
+            s += '\n'.join(LOG_BUFFER)
+        proxy.reportCrash_withGithubUrl_(s, github_url)
 
-def install_exception_hook():
     sys.excepthook = report_crash
+
+# A global log buffer to use for error reports
+LOG_BUFFER = []
 
 class CocoaHandler(logging.Handler):
     def emit(self, record):
-        proxy.log_(record.getMessage())
+        msg = record.getMessage()
+        proxy.log_(msg)
+        LOG_BUFFER.append(msg)
+        del LOG_BUFFER[:-20]
 
 def install_cocoa_logger():
     logging.getLogger().addHandler(CocoaHandler())
@@ -115,5 +113,6 @@ def patch_threaded_job_performer():
     # _async_run, under cocoa, has to be run within an autorelease pool to prevent leaks.
     # You only need this patch is you use one of CocoaProxy's function (which allocate objc
     # structures) inside a threaded job.
-    from jobprogress.performer import ThreadedJobPerformer
+    from hscommon.jobprogress.performer import ThreadedJobPerformer
     ThreadedJobPerformer._async_run = autoreleasepool(ThreadedJobPerformer._async_run)
+
