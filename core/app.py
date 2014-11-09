34bb2a23da16fcc2ea736c50eedc3dad4e5ef74a
Virgil Dupras <hsoft@hardcoded.net>
2014-11-09 10:59:13 -0500

    v2.8.0

--- core/app.py
+++ core/app.py
@@ -139,7 +139,7 @@ class Application(Broadcaster):
     APP_NAME = "moneyGuru"
     PROMPT_NAME = APP_NAME
     NAME = APP_NAME
-    VERSION = '2.7.2'
+    VERSION = '2.8.0'
 
     def __init__(
             self, view, date_format='dd/MM/yyyy', decimal_sep='.', grouping_sep='',
