7c5bcfae8335296dc353681851f4e0b874b264d4
Virgil Dupras <hsoft@hardcoded.net>
2014-12-28 11:28:00 -0500

    Update qtlib

--- qtlib/error_report_dialog.py
+++ qtlib/error_report_dialog.py
@@ -1,26 +1,28 @@
 # Created By: Virgil Dupras
 # Created On: 2009-05-23
 # Copyright 2014 Hardcoded Software (http://www.hardcoded.net)
-# 
-# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+#
+# This software is licensed under the "BSD" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.hardcoded.net/licenses/bsd_license
 
 import traceback
 import sys
 import os
 
-from PyQt4.QtCore import Qt, QUrl, QCoreApplication, QSize
-from PyQt4.QtGui import (QDialog, QDesktopServices, QVBoxLayout, QHBoxLayout, QLabel,
-    QPlainTextEdit, QPushButton, QApplication)
+from PyQt4.QtCore import Qt, QCoreApplication, QSize
+from PyQt4.QtGui import (
+    QDialog, QVBoxLayout, QHBoxLayout, QLabel, QPlainTextEdit, QPushButton
+)
 
 from hscommon.trans import trget
+from hscommon.desktop import open_url
 from .util import horizontalSpacer
 
 tr = trget('qtlib')
 
 class ErrorReportDialog(QDialog):
-    def __init__(self, parent, error):
+    def __init__(self, parent, github_url, error):
         flags = Qt.CustomizeWindowHint | Qt.WindowTitleHint | Qt.WindowSystemMenuHint
         QDialog.__init__(self, parent, flags)
         self._setupUi()
@@ -30,59 +32,56 @@ class ErrorReportDialog(QDialog):
         # Under windows, we end up with an error report without linesep if we don't mangle it
         errorText = errorText.replace('\n', os.linesep)
         self.errorTextEdit.setPlainText(errorText)
-        
-        self.sendButton.clicked.connect(self.accept)
+        self.github_url = github_url
+
+        self.sendButton.clicked.connect(self.goToGithub)
         self.dontSendButton.clicked.connect(self.reject)
-    
+
     def _setupUi(self):
         self.setWindowTitle(tr("Error Report"))
         self.resize(553, 349)
         self.verticalLayout = QVBoxLayout(self)
         self.label = QLabel(self)
-        self.label.setText(tr("Something went wrong. Would you like to send the error report to Hardcoded Software?"))
+        self.label.setText(tr("Something went wrong. How about reporting the error?"))
         self.label.setWordWrap(True)
         self.verticalLayout.addWidget(self.label)
         self.errorTextEdit = QPlainTextEdit(self)
         self.errorTextEdit.setReadOnly(True)
         self.verticalLayout.addWidget(self.errorTextEdit)
-        msg = tr("Although the application should continue to run after this error, it may be in an "
-            "instable state, so it is recommended that you restart the application.")
+        msg = tr(
+            "Error reports should be reported as Github issues. You can copy the error traceback "
+            "above and paste it in a new issue (bonus point if you run a search to make sure the "
+            "issue doesn't already exist). What usually really helps is if you add a description "
+            "of how you got the error. Thanks!"
+            "\n\n"
+            "Although the application should continue to run after this error, it may be in an "
+            "unstable state, so it is recommended that you restart the application."
+        )
         self.label2 = QLabel(msg)
         self.label2.setWordWrap(True)
         self.verticalLayout.addWidget(self.label2)
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.addItem(horizontalSpacer())
         self.dontSendButton = QPushButton(self)
-        self.dontSendButton.setText(tr("Don\'t Send"))
+        self.dontSendButton.setText(tr("Close"))
         self.dontSendButton.setMinimumSize(QSize(110, 0))
         self.horizontalLayout.addWidget(self.dontSendButton)
         self.sendButton = QPushButton(self)
-        self.sendButton.setText(tr("Send"))
+        self.sendButton.setText(tr("Go to Github"))
         self.sendButton.setMinimumSize(QSize(110, 0))
         self.sendButton.setDefault(True)
         self.horizontalLayout.addWidget(self.sendButton)
         self.verticalLayout.addLayout(self.horizontalLayout)
-    
-    def accept(self):
-        text = self.errorTextEdit.toPlainText()
-        url = QUrl("mailto:support@hardcoded.net?SUBJECT=Error Report&BODY=%s" % text)
-        QDesktopServices.openUrl(url)
-        QDialog.accept(self)
-    
 
-def install_excepthook():
+    def goToGithub(self):
+        open_url(self.github_url)
+
+
+def install_excepthook(github_url):
     def my_excepthook(exctype, value, tb):
         s = ''.join(traceback.format_exception(exctype, value, tb))
-        dialog = ErrorReportDialog(None, s)
+        dialog = ErrorReportDialog(None, github_url, s)
         dialog.exec_()
-    
+
     sys.excepthook = my_excepthook
 
-if __name__ == '__main__':
-    app = QApplication([])
-    QCoreApplication.setOrganizationName('Hardcoded Software')
-    QCoreApplication.setApplicationName('FooApp')
-    QCoreApplication.setApplicationVersion('1.2.3')
-    dialog = ErrorReportDialog(None, 'some traceback')
-    dialog.show()
-    sys.exit(app.exec_())
