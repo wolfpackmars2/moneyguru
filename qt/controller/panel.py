48d0ca275fef158db2f632e6f0ad08ac4e2839aa
Virgil Dupras <hsoft@hardcoded.net>
2015-07-18 09:28:48 -0400

    Remove unused import

--- qt/controller/panel.py
+++ qt/controller/panel.py
@@ -8,7 +8,7 @@
 
 from PyQt5.QtCore import Qt, QSignalMapper
 from PyQt5.QtWidgets import (
-    QWidget, QDialog, QLineEdit, QSpinBox, QComboBox, QCheckBox, QPlainTextEdit, QDialogButtonBox
+    QWidget, QDialog, QLineEdit, QSpinBox, QComboBox, QCheckBox, QPlainTextEdit
 )
 
 class Panel(QDialog):
