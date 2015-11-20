b2ea65c6fd6e6703720aa4be56f3f8e3375232da
Virgil Dupras <hsoft@hardcoded.net>
2015-11-19 20:34:04 -0500

    qt: fix Account Lookup up/down key shortcuts
    I have no idea when they stopped working, but it makes sense that they
    didn't work since the `QShortcut` target was wrong.
    
    fixes #444

--- qt/controller/lookup.py
+++ qt/controller/lookup.py
@@ -1,9 +1,9 @@
 # Created By: Virgil Dupras
 # Created On: 2010-02-26
 # Copyright 2015 Hardcoded Software (http://www.hardcoded.net)
-# 
-# This software is licensed under the "GPLv3" License as described in the "LICENSE" file, 
-# which should be included with this package. The terms are also available at 
+#
+# This software is licensed under the "GPLv3" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
 # http://www.gnu.org/licenses/gpl-3.0.html
 
 from PyQt4 import QtGui
@@ -17,20 +17,20 @@ tr = trget('ui')
 
 class Lookup(QWidget):
     MODEL_CLASS = None
-    
+
     def __init__(self, parent, model):
         QWidget.__init__(self, parent, Qt.Window)
         self.model = model
         self.model.view = self
         self._setupUi()
-        
+
         self.searchEdit.searchChanged.connect(self.searchChanged)
         self.searchEdit.returnPressed.connect(self.returnPressed)
         self.namesList.currentRowChanged.connect(self.currentRowChanged)
         self.namesList.itemDoubleClicked.connect(self.itemDoubleClicked)
         self._shortcutUp.activated.connect(self.upPressed)
         self._shortcutDown.activated.connect(self.downPressed)
-    
+
     def _setupUi(self):
         self.setWindowTitle(tr("Lookup"))
         self.resize(314, 331)
@@ -46,48 +46,48 @@ class Lookup(QWidget):
 
         self.searchEdit.immediate = True
         seq = QKeySequence(Qt.Key_Up)
-        self._shortcutUp = QShortcut(seq, self, None, None, Qt.WidgetShortcut)
+        self._shortcutUp = QShortcut(seq, self.searchEdit, None, None, Qt.WidgetShortcut)
         seq = QKeySequence(Qt.Key_Down)
-        self._shortcutDown = QShortcut(seq, self, None, None, Qt.WidgetShortcut)
-    
+        self._shortcutDown = QShortcut(seq, self.searchEdit, None, None, Qt.WidgetShortcut)
+
     def _restoreSelection(self):
         self.namesList.setCurrentRow(self.model.selected_index)
-    
+
     #--- Event Handlers
     def returnPressed(self):
         self.model.go()
-    
+
     def searchChanged(self):
         self.model.search_query = str(self.searchEdit.text())
-    
+
     def currentRowChanged(self, row):
         if row >= 0:
             self.model.selected_index = row
-    
+
     def itemDoubleClicked(self, item):
         self.model.go()
-    
+
     def upPressed(self):
         if self.namesList.currentRow() > 0:
             self.namesList.setCurrentRow(self.namesList.currentRow()-1)
-    
+
     def downPressed(self):
         if self.namesList.currentRow() < self.namesList.count()-1:
             self.namesList.setCurrentRow(self.namesList.currentRow()+1)
-    
+
     #--- model --> view
     def refresh(self):
         self.namesList.clear()
         self.namesList.addItems(self.model.names)
         self._restoreSelection()
         self.searchEdit.setText(self.model.search_query)
-    
+
     def show(self):
         QWidget.show(self)
         self.searchEdit.setFocus()
         # see csv_options
         self.raise_()
-    
+
     def hide(self):
         QWidget.hide(self)
-    
+
